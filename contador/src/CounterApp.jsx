e054e7c3a8a56df28b5976e213a85bc7eeba2560
Diego Arroyo Palacios <89891231+DiegoArroyo12@users.noreply.github.com>
2026-05-17 13:32:13 -0600

    Aplicación Contador (useState)

---
+++ contador/src/CounterApp.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export const CounterApp = ({value}) => {
+
+  const [counter, setCounter] = useState(value);
+  const counterIncrement = () => setCounter(counter + 1);
+
+  return <>
+    <h2>El valor del contador es { counter }</h2>
+    <button onClick={counterIncrement}>Incrementar Contador +1</button>
+  </>
+}
